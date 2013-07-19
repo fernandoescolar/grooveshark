e6c9af038496c34399a2e240a29d3a693af4007c
fernandoescolar <fer.escolar@gmail.com>
2013-07-19 14:17:29 +0200

    old code deleted

--- Xaml/ViewDefinitions/SecondViewBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GrooveSharp.Xaml.ViewDefinitions
-{
-    using Techdencias.Mvvm.Metro.Base;
-    using ViewModels;
-
-    public abstract class SecondViewBase : ViewBase<SecondViewModel>
-    {
-    }
-}
